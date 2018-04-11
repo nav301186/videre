99330936886649f0aab200fc30a94069c5bd287b
NAVEEN <nav3011186@gmail.com>
2018-04-11 07:05:18 +0200

    creating card for each team member

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,70 +18,82 @@ import * as _ from 'underscore';
 export class AppComponent {
   title = 'app';
 
+  pairs_groups = [];
+
   constructor(private matrixService: MatrixService) {
   }
 
   totalNumberOfDaysPaired(stats : Stat[], name: string){
+    let total_number_of_days_paired = stats.map(stat => stat.days_paired).reduce((c,p) => c + p);
     let stats_for_member = stats.filter(stat => stat.first_pair === name || stat.second_pair == name).map(stat => stat.days_paired);
-    return stats_for_member.reduce((c, p) => c + p)
+    let i_p =  stats_for_member.reduce((c, p) => c + p)
+    return (i_p/total_number_of_days_paired)*100
   }
 
   ngAfterViewInit() {
     this.matrixService.dummyData()
       .subscribe(result => {
         // result = _.take(result, result.length/2)
-        let uniquePairs = result.map(stat => stat.first_pair);
-        result = _.sortBy(result, function(o) {o.days_paired })
-        // let halfResult = _.take(result, result.length/2);
-        console.log(result.length)
-        let labels = result.map(stat => {
-          if (stat.first_pair == stat.second_pair) {
-            return stat.first_pair;
-          }
-          return stat.first_pair + " & " + stat.second_pair
+        let all_names = [];
+
+        result.forEach(r => {
+          all_names.push(r.first_pair);
+          all_names.push(r.second_pair);
         });
 
-        let values = result.map(stat => stat.days_paired);
+        let uniquePairs = _.uniq(all_names);
 
-        let all_sets = [];
+        result = _.sortBy(result, function(o) {o.days_paired })
+
+        this.pairs_groups = _.map(uniquePairs, function(name) {
+          return {name: name,  pairs: _.filter(result, r => r.first_pair == name || r.second_pair == name) }})
+
+      let cardGroup = document.createElement('div');
+      cardGroup.className = 'card-columns';
+      document.getElementsByTagName('body')[0].appendChild(cardGroup);
+
+        this.pairs_groups.forEach(pair => this.drawChart(pair))
+      });
+  }
+
+    drawChart(group){
+      let result = group.pairs;
+      let all_sets  = [];
+      console.log(group)
         let sets = result.forEach(stat => {
           if(stat.first_pair != stat.second_pair){
-            //People who have paired more should have a bigger circle
-             all_sets.push({ sets: [stat.first_pair], size: this.totalNumberOfDaysPaired(result, stat.first_pair)});
+             all_sets.push({ sets: [stat.first_pair], size: this.totalNumberOfDaysPaired(result, stat.first_pair) });
              all_sets.push({ sets: [stat.second_pair], size: this.totalNumberOfDaysPaired(result, stat.second_pair)});
             all_sets.push({ sets: [stat.first_pair, stat.second_pair], size: stat.days_paired });
           }
-          else {
-          // all_sets.push({ sets: [stat.first_pair], size: this.totalNumberOfDaysPaired(result, stat.first_pair)*3});
-          }
-        });
+        })
+
+      let id =  group.name;
+      let iDiv = document.createElement('card');
+      iDiv.id = id;
+      iDiv.className = 'card pull-left';
+      document.getElementsByClassName('card-columns')[0].appendChild(iDiv);
 
         let chart = venn.VennDiagram()
-          .wrap(false)
-          .fontSize("16px")
-          .width(1200)
-          .height(1000);
+        .wrap(false)
+        .width(450)
+        .height(450)
 
-        d3.select("#venn").datum(all_sets).call(chart);
+      let div_id = "#" + id
+
+        d3.select(div_id).datum(all_sets).call(chart);
         var colours = ['red', 'green', 'blue', "yellow", "yellow", ,"skyblue", "brown", "magenta"];
-
-        // d3.selectAll("#venn .venn-circle path")
-        // .style("fill-opacity", 0)
-        //   .style("stroke-width", 10)
-        //   .style("stroke-opacity", 1);
-
-         d3.selectAll("#venn .venn-circle path")
-                .style("fill-opacity", 0.1)
-                .style("stroke-width", 10)
+      // document.getElementsByClassName("card").style.color = "red";
+         d3.selectAll(div_id + " .venn-circle path")
+                .style("fill-opacity", 0.2)
+                .style("stroke-width", 8)
                 .style("stroke-opacity", .5)
-                .style("stroke", function(d,i) { return colours[i%8]; });
+                .style("left-margin", 0)
+                .style("stroke", function(d,i) { return colours[i%7]; });
 
-            d3.selectAll("#venn .venn-circle text")
-                .style("fill", function(d,i) { return colours[i%8]})
-                .style("font-size", "24px")
-                .style("font-weight", "100");
-      });
-
-  }
-
+            d3.selectAll(div_id + " .venn-circle text")
+                .style("fill", "black")
+                .style("font-size", "30px")
+                .style("font-weight", "300");
+    }
 }
--- src/app/matrix.service.ts
+++ src/app/matrix.service.ts
@@ -30,111 +30,147 @@ export class MatrixService {
     return JSON.parse(JSON.stringify([
     {
         "second_pair": "anup",
-        "percent": 16.901408450704224,
+        "percent": 16.363636363636363,
         "first_pair": "anup",
-        "days_paired": 12
+        "days_paired": 36
     },
     {
         "second_pair": "ben",
-        "percent": 1.4084507042253522,
+        "percent": 0.45454545454545453,
         "first_pair": "anup",
         "days_paired": 1
     },
     {
-        "second_pair": "daniel",
-        "percent": 1.4084507042253522,
+        "second_pair": "koustubh",
+        "percent": 0.9090909090909091,
         "first_pair": "anup",
-        "days_paired": 1
+        "days_paired": 2
     },
     {
         "second_pair": "nelson",
-        "percent": 4.225352112676056,
+        "percent": 2.727272727272727,
         "first_pair": "anup",
-        "days_paired": 3
+        "days_paired": 6
     },
     {
         "second_pair": "sabrina",
-        "percent": 2.8169014084507045,
+        "percent": 0.9090909090909091,
         "first_pair": "anup",
         "days_paired": 2
     },
     {
         "second_pair": "serdar",
-        "percent": 4.225352112676056,
+        "percent": 4.090909090909091,
         "first_pair": "anup",
-        "days_paired": 3
+        "days_paired": 9
     },
     {
-        "second_pair": "nelson",
-        "percent": 2.8169014084507045,
+        "second_pair": "ashish",
+        "percent": 1.8181818181818181,
+        "first_pair": "ashish",
+        "days_paired": 4
+    },
+    {
+        "second_pair": "ben",
+        "percent": 0.9090909090909091,
         "first_pair": "ashish",
         "days_paired": 2
     },
     {
-        "second_pair": "ben",
-        "percent": 7.042253521126761,
-        "first_pair": "ben",
-        "days_paired": 5
-    },
-    {
-        "second_pair": "koustubh",
-        "percent": 2.8169014084507045,
-        "first_pair": "ben",
+        "second_pair": "nelson",
+        "percent": 0.9090909090909091,
+        "first_pair": "ashish",
         "days_paired": 2
     },
     {
         "second_pair": "sabrina",
-        "percent": 1.4084507042253522,
-        "first_pair": "ben",
-        "days_paired": 4
-    },
-    {
-        "second_pair": "serdar",
-        "percent": 2.8169014084507045,
-        "first_pair": "ben",
-        "days_paired": 2
-    },
-    {
-        "second_pair": "anup",
-        "percent": 5.633802816901409,
-        "first_pair": "koustubh",
-        "days_paired": 4
-    },
-    {
-        "second_pair": "nelson",
-        "percent": 1.4084507042253522,
-        "first_pair": "daniel",
+        "percent": 0.45454545454545453,
+        "first_pair": "ashish",
         "days_paired": 1
     },
     {
-        "second_pair": "ashish",
-        "percent": 7.042253521126761,
-        "first_pair": "anup",
-        "days_paired": 5
+        "second_pair": "serdar",
+        "percent": 0.45454545454545453,
+        "first_pair": "ashish",
+        "days_paired": 1
+    },
+    {
+        "second_pair": "ben",
+        "percent": 6.8181818181818175,
+        "first_pair": "ben",
+        "days_paired": 15
+    },
+    {
+        "second_pair": "koustubh",
+        "percent": 1.8181818181818181,
+        "first_pair": "ben",
+        "days_paired": 4
     },
     {
         "second_pair": "nelson",
-        "percent": 1.4084507042253522,
-        "first_pair": "koustubh",
-        "days_paired": 10
-    },
-    {
-        "second_pair": "nelson",
-        "percent": 14.084507042253522,
-        "first_pair": "nelson",
-        "days_paired": 10
+        "percent": 0.45454545454545453,
+        "first_pair": "ben",
+        "days_paired": 1
     },
     {
         "second_pair": "sabrina",
-        "percent": 7.042253521126761,
-        "first_pair": "sabrina",
+        "percent": 0.45454545454545453,
+        "first_pair": "ben",
+        "days_paired": 1
+    },
+    {
+        "second_pair": "serdar",
+        "percent": 1.3636363636363635,
+        "first_pair": "ben",
+        "days_paired": 3
+    },
+    {
+        "second_pair": "koustubh",
+        "percent": 6.8181818181818175,
+        "first_pair": "koustubh",
+        "days_paired": 15
+    },
+    {
+        "second_pair": "nelson",
+        "percent": 2.272727272727273,
+        "first_pair": "koustubh",
         "days_paired": 5
     },
     {
         "second_pair": "serdar",
-        "percent": 15.492957746478872,
+        "percent": 0.45454545454545453,
+        "first_pair": "koustubh",
+        "days_paired": 1
+    },
+    {
+        "second_pair": "nelson",
+        "percent": 23.18181818181818,
+        "first_pair": "nelson",
+        "days_paired": 51
+    },
+    {
+        "second_pair": "sabrina",
+        "percent": 0.9090909090909091,
+        "first_pair": "nelson",
+        "days_paired": 2
+    },
+    {
+        "second_pair": "serdar",
+        "percent": 3.6363636363636362,
+        "first_pair": "nelson",
+        "days_paired": 8
+    },
+    {
+        "second_pair": "sabrina",
+        "percent": 4.545454545454546,
+        "first_pair": "sabrina",
+        "days_paired": 10
+    },
+    {
+        "second_pair": "serdar",
+        "percent": 17.272727272727273,
         "first_pair": "serdar",
-        "days_paired": 11
+        "days_paired": 38
     }
 ]))
   }
